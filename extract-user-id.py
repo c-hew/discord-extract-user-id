4443a29ac7bd816335627a175f253fe9d95b4ae6
Chew Jian Yue <owlg@users.noreply.github.com>
2021-01-03 04:38:07 +0800

    New update v0.0.1
    Introduces automatic loop through all files in the directory and support for both author name and user-id

--- extract-user-id.py
+++ extract-user-id.py
@@ -1,29 +1,23 @@
 import re 
-
-file = "*.html"
-
+import time
+startTime = time.time()
+from os import listdir
+from os.path import isfile, join
+mypath = "./"
 infilename = "user_id_new.txt"
 outfilename = "user_id_new_1.txt"
-
-with open(file, "r", encoding="utf-8") as f:
-    fin = f.read()
-    output = re.findall("data-user-id=\"(.*?)\"", fin)
-    print(output)
-
-fout = open(infilename, "a", encoding='utf-8')
-for item in output:
-    fout.write(item)
-    fout.write("\n")
-
+fout = open(infilename, "a", encoding='UTF8')
+for file in listdir(mypath):
+    if isfile(join(mypath, file)) and file.endswith('.html'):
+        with open(file, "r", encoding="utf-8") as f:
+            fin = f.read()
+            output = re.findall("data-user-id=\"(.*?)\"", fin)
+            usertag = re.findall("class=\"chatlog__author-name\" title=\"(.*?)\"", fin)
+            new_obj = list(zip(output, usertag))
+        for item in new_obj:
+            fout.write(item[0])
+            fout.write(":")
+            fout.write(item[1])
+            fout.write("\n")
 fout.close()
-
-lines_seen = set()
-outfile = open(outfilename, "w")
-for line in open(infilename, "r"):
-    if line not in lines_seen:
-        outfile.write(line)
-        lines_seen.add(line)
-outfile.close()
-
-count = len(open(outfilename).readlines(  ))
-print("Number of user IDs are {}".format(count))
+print("It took {0:2f} seconds to finish running.".format(time.time() - startTime))
